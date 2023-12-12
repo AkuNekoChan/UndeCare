1eb0a335aa781fa2a15b5aa5dd3cfecad8122a82
AkuNekoChan <40806142+AkuNekoChan@users.noreply.github.com>
2023-12-12 03:23:58 -0300

    Add files via upload

---
+++ js/escolha.js
@@ -0,0 +1,4 @@
+document.querySelector('form').addEventListener('submit', function() {
+    alert('Cadastro de Cuidador realizado com sucesso!'); // Altere isso conforme necessário
+    window.location.href = '../php/listar_usuario.php'; // Substitua 'nova_pagina_cuidador.php' pelo nome real da sua nova página
+});
